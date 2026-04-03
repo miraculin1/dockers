4af7f65089360421f0cc53619a5b6a42b9d4199c
miraculin <2855193562@qq.com>
2026-04-03 22:36:49 +0800

    test on debian13-xin, mount libs into container

--- cuda11_8_ROS/run_docker_gui.sh
+++ cuda11_8_ROS/run_docker_gui.sh
@@ -21,6 +21,13 @@ docker run -it \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=$DISPLAY \
   --env="QT_X11_NO_MITSHM=1" \
+  -e NVIDIA_DRIVER_CAPABILITIES=all \
+  -v /usr/lib/x86_64-linux-gnu/libnvidia-glcore.so.550.163.01:/usr/lib/x86_64-linux-gnu/libnvidia-glcore.so.550.163.01 \
+  -v /usr/lib/x86_64-linux-gnu/libnvidia-tls.so.550.163.01:/usr/lib/x86_64-linux-gnu/libnvidia-tls.so.550.163.01 \
+  -v /usr/lib/x86_64-linux-gnu/libnvidia-glsi.so.550.163.01:/usr/lib/x86_64-linux-gnu/libnvidia-glsi.so.550.163.01 \
+  -v /usr/lib/x86_64-linux-gnu/libnvidia-gpucomp.so.550.163.01:/usr/lib/x86_64-linux-gnu/libnvidia-gpucomp.so.550.163.01 \
+  -v /usr/lib/x86_64-linux-gnu/libnvidia-gpucomp.so.550.163.01:/usr/lib/x86_64-linux-gnu/libnvidia-gpucomp.so.0 \
+  -e LIBGL_ALWAYS_INDIRECT=0 \
   --env="NVIDIA_DRIVER_CAPABILITIES=all" \
   --env="NVIDIA_VISIBLE_DEVICES=all" \
   --device /dev:/dev \
