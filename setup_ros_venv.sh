97b85bde2898d6a207afbb551ff4d115138df9e9
miraculin <2855193562@qq.com>
2026-05-21 21:25:26 +0800

    add conda venv ros setup script

---
+++ setup_ros_venv.sh
@@ -0,0 +1,13 @@
+pip install \
+  catkin_tools \
+  catkin_pkg \
+  rospkg \
+  rosdep \
+  rosdistro \
+  empy==3.3.4 \
+  PyYAML \
+  numpy \
+  defusedxml \
+  netifaces \
+  distro \
+  osrf-pycommon
